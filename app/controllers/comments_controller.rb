716b7d19ccfd87dc3c8f145e4f2ff203a0561c7f
ganesh kumar <ganeshtheone@gmail.com>
2010-10-28 19:20:33 +0530

    login check added to delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,9 @@
 class CommentsController < ApplicationController
-	def create
+       
+       before_filter :authenticate, :only => :destroy
+       
+       def create
+		@post = Post.find(params[:post_id])
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.create(params[:comment])
 		redirect_to post_path(@post)
@@ -12,7 +16,7 @@ class CommentsController < ApplicationController
 	
 	def update
 		@post=Post.find(params[:post_id])
-		@comment = @post.comments(params[:id])
+		@comment = @post.comments.find(params[:id])
 		if @comment.update_attributes(params[:comment])
 		     flash[:notice] = "Post updated successfully"
 		     redirect_to @post
