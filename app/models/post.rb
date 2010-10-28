000fc0da015fafddc98392bf1dc170e9e2eea94d
ganesh kumar <ganeshtheone@gmail.com>
2010-10-28 17:33:55 +0530

    added comments views,update,delete

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,11 +7,4 @@ field :content, :type=>String
 embeds_many :comments
 end				
 
-=begin
-class Comments
-  include Mongoid::Document
-  include Mongoid::Timestamps
-	field :name, :type=>String
-	field :comment, :type=>String
-	embedded_in :Post,:inverse_of => :comments
-=end
+
